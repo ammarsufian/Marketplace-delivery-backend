0e8ae4fb3858760497fd68793eb3e4d5b35d06b9
ammarsufian60 <ammarsufian60@gmail.com>
2022-08-30 09:57:52 +0300

    Apply the following changes:
    1- initialize the packages feature

---
+++ database/migrations/2022_08_29_095536_create_packages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->json('name');
+            $table->json('description');
+            $table->decimal('price');
+            $table->unsignedInteger('covered_order_counts');
+            $table->unsignedInteger('covered_month_counts');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
