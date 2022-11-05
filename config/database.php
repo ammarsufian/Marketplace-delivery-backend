9ee0d8f7ff6fb294d268bc485954e5f35cdf7368
ammarsufian60 <ammarsufian60@gmail.com>
2022-11-05 15:52:35 +0300

    Apply the following changes:
    1-Handle Blogger System inside the same nova admin
    
    Please Put these's variables inside your env file
    `
    BLOGGER_DATABASE_HOST=127.0.0.1
    BLOGGER_DATABASE_PORT=3306
    BLOGGER_DATABASE=blogger
    BLOGGER_DATABASE_USERNAME=ammar
    BLOGGER_DATABASE_PASSWORD=rootroot
    `

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'blogger' => [
+            'driver' => 'mysql',
+            'url' => env('BLOGGER_DATABASE_URL'),
+            'host' => env('BLOGGER_DATABASE_HOST', '127.0.0.1'),
+            'port' => env('BLOGGER_DATABASE_PORT', '3306'),
+            'database' => env('BLOGGER_DATABASE', 'forge'),
+            'username' => env('BLOGGER_DATABASE_USERNAME', 'forge'),
+            'password' => env('BLOGGER_DATABASE_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'legacy' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
